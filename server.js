51a4a0d9b55437153ee471ef4ab62bd2be5e35af
AidenMiller123 <123018143+AidenMiller123@users.noreply.github.com>
2023-04-24 19:23:30 -0400

    Added notes in server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection(
     host: 'localhost',
     // MySQL username,
     user: 'root',
-    // TODO: Add MySQL password here
+    // MySQL password
     password: 'Miller1676$$',
     database: 'employee_db'
   },
@@ -23,7 +23,7 @@ const db = mysql.createConnection(
 );
 
 
-
+// variable containing the inquirer prompt for Add Department 
 const addDepartment = [{
   type: 'input',
   name: 'addDepartment',
@@ -31,7 +31,7 @@ const addDepartment = [{
 }]
 
 
-
+// variable containing the inquirer prompt for Add Employee 
 const addEmployee = [{
 
   type: 'input',
@@ -42,11 +42,8 @@ const addEmployee = [{
   type: 'input',
   name: 'employeeLastName',
   message: "What is the employee's last name?"
-},
-
-]
-
-
+}]
+// variable containing the inquirer prompt for Add Role
 const addRole = [{
   type: 'input',
   name: 'roleName',
@@ -59,8 +56,9 @@ const addRole = [{
 },
 ]
 
-
+// questions function that holds all the inquirer prompts and responses
 const questions = () => {
+  // homepage variable that holds the inquirer prompt for the main list of questions
   const homePage = [{
     type: 'list',
     name: 'homepage',
@@ -77,27 +75,34 @@ const questions = () => {
     ],
     initial: 1
   }]
-
+// Inqiurer prompt that starts the questions 
   inquirer
     .prompt(homePage)
     .then((response) => {
+      // If statement for if the user selects View All Employees
       if (response.homepage === 'View All Employees') {
+        // sql query that gets all the the employees and tables the results
         const sql = "Select a.id, a.first_name, a.last_name, r.title AS title, d.name AS department, r.salary AS salary, concat(b.first_name,' ', b.last_name) as manager from employee a left join employee b ON (a.manager_id = b.id) join role r ON (a.role_id = r.id) join department d ON (r.department_id = d.id)"
         db.query(sql, function (err, results) {
           console.table(results)
           if (err) {
             results.status(500).json({ error: err.message });
           }
+          // returns the function question to start over function
           return questions();
         });
       }
+       // If statement for if the user selects Add Employee
       else if (response.homepage === 'Add Employee') {
+        // Inquirer for the addEmployee prompts
         inquirer
           .prompt(addEmployee)
           .then((response => {
+            // stores responses in an array
             const arr = [response.employeeFirstName, response.employeeLastName]
-
+            // Gets all roles
             db.query('SELECT * FROM role', (err, res) => {
+              // Prompts user with the roles table using id and title
               const roles = res.map(({ id, title }) => ({ name: title, value: id }))
               inquirer
                 .prompt([
@@ -109,9 +114,12 @@ const questions = () => {
                   }
                 ])
                 .then(response => {
+                  // Pushes response into the array containing other responses
                   const role = response.employeeRole
                   arr.push(role)
+                  // gets all managers
                   db.query("SELECT id, concat(first_name,' ',last_name) AS name FROM employee WHERE manager_id IS NULL;", (err, res) => {
+                    // Prompts user with the managers using the name and id
                     const managers = res.map(({ name, id }) => ({ name: name, value: id }))
                     inquirer
                       .prompt([
@@ -123,13 +131,16 @@ const questions = () => {
                         }
                       ])
                       .then(response => {
+                        // Pushes response into the array containing other responses
                         const manager = response.employeeManager
                         arr.push(manager)
+                        // Adds new employee to the employee table
                         db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`, arr, function (err, results) {
                           if (err) {
                             response.status(500).json({ error: err.message });
                           }
                           console.log(`Added employee to the database`);
+                          // returns the function question to start over function
                           return questions();
                         });
                       });
@@ -139,10 +150,14 @@ const questions = () => {
           }));
 
       }
+       // If statement for if the user selects View All Employees
       else if (response.homepage === 'Update Employee Role') {
+        // Gets all employees by id and name
         db.query("SELECT id, concat(first_name,' ',last_name) AS name FROM employee", (err, res) => {
           const employees = res.map(({id, name}) => ({name: name, value: id}))
+          // Creates an empty array
           const arr =[];
+          // Prompts user with the employees table 
           inquirer 
           .prompt([
             {
@@ -153,10 +168,13 @@ const questions = () => {
             }
           ])
           .then(response => {
+            // Pushes response into the array containing other responses
             const employeeUpdate = response.employeeUpdate
             arr.push(employeeUpdate)
+            // Gets all from role table
             db.query('SELECT * FROM role', (err, res) => {
               const roles = res.map(({ id, title }) => ({ name: title, value: id }))
+              // prompts user with the roles table
               inquirer
               .prompt([
                 {
@@ -167,40 +185,49 @@ const questions = () => {
                 }
               ])
               .then(response => {
+                // Pushes response into the array containing other responses
                 const role = response.employeeRoleUpdate
                 arr.push(role)
+                // Changes the order of reesponses in the array to match query
                 let employeeUpdate = arr[0]
                 arr[0] = role
                 arr[1] = employeeUpdate
+                // Updates employee role using employee id
                 db.query(`UPDATE employee SET role_id = ? WHERE id = ?`, arr, function (err, results) {
                   if (err) {
                     response.status(500).json({ error: err.message });
                   }
                   console.log(`Updated employee in the database`);
+                  // returns the function question to start over function
                   return questions();
                 });
-              })
-            })
-          })
-        })
+              });
+            });
+          });
+        });
 
       }
+       // If statement for if the user selects View All Employees
       else if (response.homepage === 'View All Roles') {
+        // Gets all from role table 
         const sql = 'SELECT role.id AS id, role.title AS title, department.name AS department, role.salary AS salary FROM role JOIN department ON role.department_id = department.id'
         db.query(sql, function (err, results) {
           console.table(results)
           if (err) {
             results.status(500).json({ error: err.message });
           }
+          // returns the function question to start over function
           return questions();
         });
       }
+       // If statement for if the user selects View All Employees
       else if (response.homepage === 'Add Role') {
         inquirer
           .prompt(addRole)
           .then((response => {
+            // creates an array with the responses
             const arr = [response.roleName, response.roleSalary]
-
+            // get all departments
             db.query('SELECT * FROM department', (err, res) => {
               const dep = res.map(({ id, name }) => ({ name: name, value: id }))
               inquirer
@@ -213,32 +240,39 @@ const questions = () => {
                   }
                 ])
                 .then(response => {
+                  // Pushes response into the array containing other responses
                   const department = response.roleDepartment
                   arr.push(department);
+                  // adds new role into role table
                   db.query(`INSERT INTO role (title, salary, department_id) VALUES (?,?,?)`, arr, function (err, results) {
                     if (err) {
                       response.status(500).json({ error: err.message });
                     }
                     console.log(`Added role to the database`);
+                    // returns the function question to start over function
                     return questions();
                   })
                 })
             })
           }))
       }
+       // If statement for if the user selects View All Employees
       else if (response.homepage === 'View All Departments') {
         db.query('SELECT * FROM department', function (err, results) {
           console.table(results)
           if (err) {
             results.status(500).json({ error: err.message });
           }
+          // returns the function question to start over function
           return questions();
         })
       }
+       // If statement for if the user selects View All Employees
       else if (response.homepage === 'Add Department') {
         inquirer
           .prompt(addDepartment)
           .then((response) => {
+            // adds new department into department able
             const sql = `INSERT INTO department (name) VALUES (?)`
             const vaules = [response.addDepartment];
             db.query(sql, vaules, function (err, results) {
@@ -246,10 +280,11 @@ const questions = () => {
                 results.status(500).json({ error: err.message });
               }
               console.log(`Added ${response.addDepartment} to the database`);
+              // returns the function question to start over function
               return questions();
             })
           })
-      }
+      } // If statement for if the user selects View All Employees
       else if (response.homepage === 'Quit') {
         process.exit(app)
       };
@@ -257,7 +292,7 @@ const questions = () => {
     });
 
 };
-
+// Calls the function containing inquirer prompts
 questions();
 
 
